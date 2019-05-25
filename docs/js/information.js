ab9a225a2bf1b3d6bca48e86a9afaa2536b5370d
willy0077 <willy633526@gmail.com>
2019-05-26 02:15:51 +0800

    initial

---
+++ docs/js/information.js
@@ -0,0 +1,51 @@
+var vm = new Vue({
+    el: '#bookapp',
+    data: function () {
+        return {
+            books: '',
+            // showbook: '',
+            // hover: false,
+
+        }
+    },
+    created() {
+        axios
+            .get('../asset/json/bookdata.json')
+            .then(val => {
+                this.books = val.data
+            });
+        // axios
+        //     .get('../asset/json/bookshow.json')
+        //     .then(val => {
+        //         this.showbook = val.data
+        //     });
+    },
+    computed: {},
+    methods: {
+        mouseover: function (index) {
+            console.log(index);
+            this.books[index].isactive = true;
+        },
+        mouseleave: function (index) {
+            this.books[index].isactive = false;
+        },
+        bookID: function (index) {
+            return "book" + index;
+        }
+        // bookfilter: function (index) {
+        //     for (let i = 0; i < this.books.length; i++) {
+        //         if (i == index) {
+        //             this.books[i].isactive = true;
+        //             this.showbook = this.books[i];
+        //         } else {
+        //             this.books[i].isactive = false;
+        //         }
+        //     }
+        // },
+    },
+});
+
+
+
+
+Vue.config.devtools = true;
---
+++ docs/js/script.js
@@ -0,0 +1,81 @@
+console.log("index");
+
+// console.clear();
+// Array.from(
+//     document.querySelectorAll('.video-tile'),
+//     function (el) {
+//         const video = el.querySelector('video');
+
+//         el.addEventListener('mouseenter', () => {
+//             video.play()
+//         });
+
+//         el.addEventListener('mouseleave', () => {
+//             video.pause();
+//             setTimeout(() => {
+//                 video.currentTime = 0
+//             }, 400);
+//         });
+//     });
+
+const elVideoToggle = document.querySelectorAll('.video-toggle');
+const videoPlayer = document.querySelectorAll('.video__player');
+
+
+const buttonbar = document.querySelector('.button');
+// const videoPause = document.getElementById('video__pause');
+// const videoPlay = document.getElementById('video__play');
+
+const videoTrack = document.querySelectorAll('.video__track')
+// const videoSound = document.getElementById('video__sound');
+// const soundTrack = document.getElementById('sound__track');
+
+
+
+function videoSetting(video, index) {
+    video.classList.toggle('active');
+    const condition = video.matches('.active');
+
+    if (condition) {
+        videoPlayer[index].play();
+    } else {
+        videoPlayer[index].pause();
+    }
+
+
+    videoTrack[index].addEventListener('change', function () {
+        var times = videoPlayer[index].duration / 100;
+        videoPlayer[index].currentTime = this.value * times;
+        console.log("videoPlayer = " + videoPlayer[index].currentTime);
+    });
+
+
+    // videoPlayer[index].addEventListener('timeupdate', function () {
+    //     videoTrack.value = videoPlayer[index].currentTime;
+    //     console.log("videoTrack.value = " + videoTrack.value);
+    // });
+}
+
+
+
+// updateVolume = function () {
+//     videoPlayer.volume = soundTrack.value / 10;
+//     console.log(videoPlayer.volume);
+// };
+
+// soundTrack.addEventListener("mousedown", function () {
+//     updateVolume();
+//     soundTrack.addEventListener("mousemove", updateVolume);
+// });
+// soundTrack.addEventListener("mouseup", function () {
+//     soundTrack.removeEventListener("mousemove", updateVolume);
+// });
+
+
+// const ipt = document.querySelector('input[type=range]')
+// ipt.onchange = function () {
+//     const val = event.target.value,
+//         progress = val / 10 * 100 + '%'
+//     console.log(progress)
+//     document.documentElement.style.setProperty('--progress', progress)
+// }
